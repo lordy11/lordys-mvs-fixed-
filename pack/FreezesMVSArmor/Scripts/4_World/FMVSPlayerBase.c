2a054b6a443ec3d4bb8dc55b50a5aea829cefd41
lordy <68857768+lordy11@users.noreply.github.com>
2026-09-11 15:21:57 +0100

    Fix DayZ 1.29 EEOnDamageCalculated parameter name

--- pack/FreezesMVSArmor/Scripts/4_World/FMVSPlayerBase.c
+++ pack/FreezesMVSArmor/Scripts/4_World/FMVSPlayerBase.c
@@ -2,26 +2,26 @@ modded class PlayerBase
 {
     protected bool m_FMVSApplyingScaledDamage;
 
-    override bool EEOnDamageCalculated(TotalDamageResult damageResult, int damageType, EntityAI source, int component, string damageZone, string ammo, vector modelPos, float speedCoef)
+    override bool EEOnDamageCalculated(TotalDamageResult damageResult, int damageType, EntityAI source, int component, string dmgZone, string ammo, vector modelPos, float speedCoef)
     {
-        if (!super.EEOnDamageCalculated(damageResult, damageType, source, component, damageZone, ammo, modelPos, speedCoef))
+        if (!super.EEOnDamageCalculated(damageResult, damageType, source, component, dmgZone, ammo, modelPos, speedCoef))
             return false;
         if (!GetGame() || !GetGame().IsServer() || m_FMVSApplyingScaledDamage)
             return true;
 
-        float blockedPercent = FMVSArmorManager.GetBlockedPercent(this, damageType, source, damageZone);
+        float blockedPercent = FMVSArmorManager.GetBlockedPercent(this, damageType, source, dmgZone);
         if (blockedPercent <= 0)
             return true;
 
         float damageMultiplier = 1.0 - (blockedPercent / 100.0);
         if (FMVSArmorManager.DebugEnabled())
-            Print(string.Format("[FreezesMVSArmor] %1 zone=%2 blockedPercent=%3 ammo=%4", GetType(), damageZone, blockedPercent, ammo));
+            Print(string.Format("[FreezesMVSArmor] %1 zone=%2 blockedPercent=%3 ammo=%4", GetType(), dmgZone, blockedPercent, ammo));
 
         if (damageMultiplier <= 0)
             return false;
 
         m_FMVSApplyingScaledDamage = true;
-        ProcessDirectDamage(damageType, source, damageZone, ammo, modelPos, damageMultiplier);
+        ProcessDirectDamage(damageType, source, dmgZone, ammo, modelPos, damageMultiplier);
         m_FMVSApplyingScaledDamage = false;
         return false;
     }
