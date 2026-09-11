9e3ea4a6e26487f0929adf310e55f576e2e6ef99
lordy <68857768+lordy11@users.noreply.github.com>
2026-09-11 18:47:55 +0100

    Create inventory size JSON on MVS startup

--- working 0.1/FreezesMVSArmor/Scripts/5_Mission/FMVSMissionServer.c
+++ working 0.1/FreezesMVSArmor/Scripts/5_Mission/FMVSMissionServer.c
@@ -4,6 +4,7 @@ modded class MissionServer
     {
         super.OnInit();
         FMVSArmorManager.Load();
+        FMVSInventorySizeManager.EnsureFile();
         GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).CallLater(FMVSArmorManager.Load, 60000, true);
     }
 };
